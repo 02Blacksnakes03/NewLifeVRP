9ef0f4fa48558154c9ab02f109f2ced5e8c1c3c3
02Blacksnakes03 <kohnke905@gmail.com>
2023-05-06 00:24:46 +0200

    add new Features

---
+++ resources/[Features]/WeihnachtsCase/IMPORT_ME.sql
@@ -0,0 +1,3 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('WeihnachtsCase', 'Case', '100', '0', '1');
+	--Edit this SQL Command if your Items Table uses other Column [Or just Copy a other Item and name the name == WeihnachtsCase and the Label the Name that shows in the Inventory]
---
+++ resources/[Features]/t1ger_garage/__resource.lua
@@ -0,0 +1,26 @@
+-------------------------------------
+------- Created by T1GER#9080 -------
+------- Cracked by 5M Support ---------
+-------------------------------------
+
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+description 'T1GER Garage'
+
+author 'T1GER#9080'
+
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'script_files/language.lua',
+	'script_files/config.lua',
+	'script_files/server.lua',
+	'protection/server.lua'
+}
+
+client_scripts {
+	'script_files/language.lua',
+	'script_files/config.lua',
+	'script_files/client.lua',
+	'script_files/utils.lua',
+	'protection/client.lua'
+}
