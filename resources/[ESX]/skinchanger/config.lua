6d808bfefa438b1939c46bfba7d751040e61b097
02Blacksnakes03 <kohnke905@gmail.com>
2023-05-06 00:19:39 +0200

    add new Features

--- resources/[ESX]/skinchanger/config.lua
+++ resources/[ESX]/skinchanger/config.lua
@@ -1,2 +1,3 @@
 Config = {}
-Config.Locale = 'de'
+
+Config.Locale = 'en'
---
+++ resources/[hud]/vs_hud/__resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+ui_page "html/html.html"
+files {
+	"html/html.html",
+	"html/*.png",
+	"html/*.js"
+}
+
+client_scripts {
+	"client.lua",
+}
+server_scripts {
+	"server.lua",
+}
