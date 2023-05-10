cf4bfab8ffda46182c5775122f0a78d1a70e0545
02Blacksnakes03 <kohnke905@gmail.com>
2023-05-10 20:28:07 +0200

    add Groups for admins

--- resources/[Admin]/ds_adminduty/config.lua
+++ resources/[Admin]/ds_adminduty/config.lua
@@ -33,6 +33,23 @@ Config.Admin = {
             ['bproof_1'] = 0,  ['bproof_2'] = 0
         }
     },
+    mina = {
+        female = {
+            ['tshirt_1'] = 14,  ['tshirt_2'] = 0,
+            ['torso_1'] = 300,   ['torso_2'] = 9,
+            ['decals_1'] = 0,   ['decals_2'] = 0,
+            ['arms'] = 8,
+            ['pants_1'] = 121,   ['pants_2'] = 9,
+            ['shoes_1'] = 82,   ['shoes_2'] = 9,
+            ['helmet_1'] = -1,  ['helmet_2'] = 0,
+            ['mask_1'] = 135,  ['mask_2'] = 9,
+            ['chain_1'] = 0,    ['chain_2'] = 0,
+            ['ears_1'] = 0,     ['ears_2'] = 0,
+            ['bags_1'] = 0,     ['bags_2'] = 0,
+            ['hair_1'] = 0,     ['hair_2'] = 0,
+            ['bproof_1'] = 0,  ['bproof_2'] = 0
+        }
+    }
     admin = {
         male = {
             ['tshirt_1'] = 15,  ['tshirt_2'] = 0,
@@ -57,7 +74,7 @@ Config.Admin = {
             ['pants_1'] = 121,   ['pants_2'] = 3,
             ['shoes_1'] = 82,   ['shoes_2'] = 3,
             ['helmet_1'] = -1,  ['helmet_2'] = 0,
-            ['mask_1'] = 153,  ['mask_2'] = 3,
+            ['mask_1'] = 135,  ['mask_2'] = 3,
             ['chain_1'] = 0,    ['chain_2'] = 0,
             ['ears_1'] = 0,     ['ears_2'] = 0,
             ['bags_1'] = 0,     ['bags_2'] = 0,
@@ -89,7 +106,7 @@ Config.Admin = {
             ['pants_1'] = 121,   ['pants_2'] = 9,
             ['shoes_1'] = 82,   ['shoes_2'] = 9,
             ['helmet_1'] = -1,  ['helmet_2'] = 0,
-            ['mask_1'] = 153,  ['mask_2'] = 9,
+            ['mask_1'] = 135,  ['mask_2'] = 4,
             ['chain_1'] = 0,    ['chain_2'] = 0,
             ['ears_1'] = 0,     ['ears_2'] = 0,
             ['bags_1'] = 0,     ['bags_2'] = 0,
@@ -121,7 +138,7 @@ Config.Admin = {
             ['pants_1'] = 121,   ['pants_2'] = 9,
             ['shoes_1'] = 82,   ['shoes_2'] = 9,
             ['helmet_1'] = -1,  ['helmet_2'] = 0,
-            ['mask_1'] = 153,  ['mask_2'] = 9,
+            ['mask_1'] = 135,  ['mask_2'] = 9,
             ['chain_1'] = 0,    ['chain_2'] = 0,
             ['ears_1'] = 0,     ['ears_2'] = 0,
             ['bags_1'] = 0,     ['bags_2'] = 0,
