68f1544fd75d813fe7841e6c1b28858e8d4abb3a
02Blacksnakes03 <kohnke905@gmail.com>
2023-05-20 14:53:52 +0200

    fix

---
+++ resources/[TestBlack]/skinchanger/fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+-- Leaked By: FiveShop https://discord.gg/KqBezd3Kpf
+game 'gta5'
+-- Leaked By: FiveShop https://discord.gg/KqBezd3Kpf
+description 'Skin Changer'
+-- Leaked By: FiveShop https://discord.gg/KqBezd3Kpf
+version '1.0.3'
+-- Leaked By: FiveShop https://discord.gg/KqBezd3Kpf
+client_scripts {
+	'locale.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/pl.lua',
+	'locales/sv.lua',
+	'locales/tr.lua',
+	'config.lua',
+	'client/main.lua'
+}
