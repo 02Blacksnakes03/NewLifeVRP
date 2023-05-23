50b8464029e75f8f0822e8307ef2d712f7785563
02Blacksnakes03 <kohnke905@gmail.com>
2023-05-23 18:35:42 +0200

    fix asaya maps

--- resources/[Maps]/gabz_vanilla_unicorn/fxmanifest.lua
+++ resources/[Maps]/gabz_vanilla_unicorn/fxmanifest.lua
@@ -1,8 +1,19 @@
-fx_version 'bodacious'
-games { 'gta5' }
-
-author 'Gabz'
-description 'Vanilla Unicorn'
-version '1.0.0'
+fx_version 'adamant'
+game 'gta5'
 
 this_is_a_map 'yes'
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+
+files '*/*/interiorproxies.meta'
+
+
+file 'stream/pillbox_hospital/gabz_timecycle_mods_1.xml'
+
+data_file 'TIMECYCLEMOD_FILE' 'stream/pillbox_hospital/gabz_timecycle_mods_1.xml'
+
+data_file 'DLC_ITYP_REQUEST' 'mosleynutt//int_corp23.ytyp'
+
+client_script {
+  "main.lua"
+}
